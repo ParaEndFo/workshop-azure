f90b7a402dfb6d0447db14bd935a9013a80a9a07
paramirez <pauloramirez106@gmail.com>
2023-05-11 09:19:57 -0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.example.servicebus.QueueManager;
+import org.example.servicebus.TopicManager;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        QueueManager.sendMessage();
+        QueueManager.sendMessageBatch();
+        QueueManager.receiveMessages();
+
+        TopicManager.sendMessage();
+        TopicManager.sendMessageBatch();
+        TopicManager.receiveMessages();
+
+    }
+}
---
+++ src/main/java/org/example/servicebus/TopicManager.java
@@ -0,0 +1,161 @@
+package org.example.servicebus;
+
+import com.azure.identity.*;
+import com.azure.messaging.servicebus.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class TopicManager {
+
+    static String topicName = "<TOPIC NAME>";
+    static String subName = "<SUBSCRIPTION NAME>";
+
+    public static void sendMessage()
+    {
+        // create a token using the default Azure credential
+        DefaultAzureCredential credential = new DefaultAzureCredentialBuilder()
+                .build();
+
+        ServiceBusSenderClient senderClient = new ServiceBusClientBuilder()
+                .fullyQualifiedNamespace("NAMESPACENAME.servicebus.windows.net")
+                .credential(credential)
+                .sender()
+                .topicName(topicName)
+                .buildClient();
+
+        // send one message to the topic
+        senderClient.sendMessage(new ServiceBusMessage("Hello, World!"));
+        System.out.println("Sent a single message to the topic: " + topicName);
+    }
+
+    public static void sendMessageBatch()
+    {
+        // create a token using the default Azure credential
+        DefaultAzureCredential credential = new DefaultAzureCredentialBuilder()
+                .build();
+
+        ServiceBusSenderClient senderClient = new ServiceBusClientBuilder()
+                .fullyQualifiedNamespace("NAMESPACENAME.servicebus.windows.net")
+                .credential(credential)
+                .sender()
+                .topicName(topicName)
+                .buildClient();
+
+        // Creates an ServiceBusMessageBatch where the ServiceBus.
+        ServiceBusMessageBatch messageBatch = senderClient.createMessageBatch();
+
+        // create a list of messages
+        List<ServiceBusMessage> listOfMessages = createMessages();
+
+        // We try to add as many messages as a batch can fit based on the maximum size and send to Service Bus when
+        // the batch can hold no more messages. Create a new batch for next set of messages and repeat until all
+        // messages are sent.
+        for (ServiceBusMessage message : listOfMessages) {
+            if (messageBatch.tryAddMessage(message)) {
+                continue;
+            }
+
+            // The batch is full, so we create a new batch and send the batch.
+            senderClient.sendMessages(messageBatch);
+            System.out.println("Sent a batch of messages to the topic: " + topicName);
+
+            // create a new batch
+            messageBatch = senderClient.createMessageBatch();
+
+            // Add that message that we couldn't before.
+            if (!messageBatch.tryAddMessage(message)) {
+                System.err.printf("Message is too large for an empty batch. Skipping. Max size: %s.", messageBatch.getMaxSizeInBytes());
+            }
+        }
+
+        if (messageBatch.getCount() > 0) {
+            senderClient.sendMessages(messageBatch);
+            System.out.println("Sent a batch of messages to the topic: " + topicName);
+        }
+
+        //close the client
+        senderClient.close();
+    }
+
+    public static void receiveMessages() throws InterruptedException
+    {
+        CountDownLatch countdownLatch = new CountDownLatch(1);
+
+        DefaultAzureCredential credential = new DefaultAzureCredentialBuilder()
+                .build();
+
+        // Create an instance of the processor through the ServiceBusClientBuilder
+        ServiceBusProcessorClient processorClient = new ServiceBusClientBuilder()
+                .fullyQualifiedNamespace("NAMESPACENAME.servicebus.windows.net")
+                .credential(credential)
+                .processor()
+                .topicName(topicName)
+                .subscriptionName(subName)
+                .processMessage(TopicManager::processMessage)
+                .processError(context -> processError(context, countdownLatch))
+                .buildProcessorClient();
+
+        System.out.println("Starting the processor");
+        processorClient.start();
+
+        TimeUnit.SECONDS.sleep(10);
+        System.out.println("Stopping and closing the processor");
+        processorClient.close();
+    }
+
+    private static List<ServiceBusMessage> createMessages()
+    {
+        // create a list of messages and return it to the caller
+        ServiceBusMessage[] messages = {
+                new ServiceBusMessage("First message"),
+                new ServiceBusMessage("Second message"),
+                new ServiceBusMessage("Third message")
+        };
+        return Arrays.asList(messages);
+    }
+
+    private static void processMessage(ServiceBusReceivedMessageContext context) {
+        ServiceBusReceivedMessage message = context.getMessage();
+        System.out.printf("Processing message. Session: %s, Sequence #: %s. Contents: %s%n", message.getMessageId(),
+                message.getSequenceNumber(), message.getBody());
+    }
+
+    private static void processError(ServiceBusErrorContext context, CountDownLatch countdownLatch) {
+        System.out.printf("Error when receiving messages from namespace: '%s'. Entity: '%s'%n",
+                context.getFullyQualifiedNamespace(), context.getEntityPath());
+
+        if (!(context.getException() instanceof ServiceBusException)) {
+            System.out.printf("Non-ServiceBusException occurred: %s%n", context.getException());
+            return;
+        }
+
+        ServiceBusException exception = (ServiceBusException) context.getException();
+        ServiceBusFailureReason reason = exception.getReason();
+
+        if (reason == ServiceBusFailureReason.MESSAGING_ENTITY_DISABLED
+                || reason == ServiceBusFailureReason.MESSAGING_ENTITY_NOT_FOUND
+                || reason == ServiceBusFailureReason.UNAUTHORIZED) {
+            System.out.printf("An unrecoverable error occurred. Stopping processing with reason %s: %s%n",
+                    reason, exception.getMessage());
+
+            countdownLatch.countDown();
+        } else if (reason == ServiceBusFailureReason.MESSAGE_LOCK_LOST) {
+            System.out.printf("Message lock lost for message: %s%n", context.getException());
+        } else if (reason == ServiceBusFailureReason.SERVICE_BUSY) {
+            try {
+                // Choosing an arbitrary amount of time to wait until trying again.
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                System.err.println("Unable to sleep for period of time");
+            }
+        } else {
+            System.out.printf("Error source %s, reason %s, message: %s%n", context.getErrorSource(),
+                    reason, context.getException());
+        }
+    }
+
+
+}
